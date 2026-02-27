112250092cd9fa51cc51776de12dddebf6ea3e45
Manavmodi0807 <Manavmodi0807@gmail.com>
2026-02-27 14:27:33 +0530

    changes in index.html,views.py,urls.py

--- payments/urls.py
+++ payments/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import index, success
+from .views import index, success, webhook
 
 urlpatterns = [
     path('', index, name="home"),
     path('success/', success, name="success"),
-    path('webhook/', webhook),
+    path('webhook/', webhook, name="webhook"),
 ]
--- payments/views.py
+++ payments/views.py
@@ -1,22 +1,22 @@
 import razorpay
 from django.conf import settings
 from django.shortcuts import render
-from django.views.decorators.csrf import csrf_exempt
-from .models import Payment
 from django.http import HttpResponse
-import json
+from .models import Payment
+
 
 client = razorpay.Client(
     auth=(settings.RAZORPAY_KEY_ID, settings.RAZORPAY_KEY_SECRET)
 )
 
+
 def index(request):
-    amount = 50000
+    amount = 50000  # ₹500 in paise
 
     order = client.order.create({
         "amount": amount,
         "currency": "INR",
-        "payment_capture": "1"
+        "payment_capture": 1
     })
 
     Payment.objects.create(
@@ -31,9 +31,9 @@ def index(request):
     })
 
 
-@csrf_exempt
 def success(request):
     if request.method == "POST":
+
         payment_id = request.POST.get("razorpay_payment_id")
         order_id = request.POST.get("razorpay_order_id")
         signature = request.POST.get("razorpay_signature")
@@ -60,27 +60,6 @@ def success(request):
 
     return render(request, "payments/failure.html")
 
-@csrf_exempt
+
 def webhook(request):
-    if request.method == "POST":
-        payload = request.body
-        signature = request.headers.get('X-Razorpay-Signature')
-
-        try:
-            client.utility.verify_webhook_signature(
-                payload,
-                signature,
-                settings.RAZORPAY_KEY_SECRET
-            )
-            data = json.loads(payload)
-
-            if data['event'] == "payment.captured":
-                order_id = data['payload']['payment']['entity']['order_id']
-                payment = Payment.objects.get(order_id=order_id)
-                payment.status = "captured"
-                payment.save()
-
-            return HttpResponse(status=200)
-
-        except:
-            return HttpResponse(status=400)
+    return HttpResponse(status=200)
